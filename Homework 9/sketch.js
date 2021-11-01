5f1914c87ac8b8f50dd9ae8e8c7d9335d65c110b
Maharg21 <89944566+Maharg21@users.noreply.github.com>
2021-11-01 00:51:11 -0600

    Add files via upload

---
+++ Homework 9/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(128, 130, 87);
+    fill('silver') 
+    rect (120,160,150,240);
+    fill(90,80,70);
+    circle (200,120,140);
+    fill('green');
+    circle (165, 100, 35);
+    fill('green');
+    circle (230, 101, 35);
+    fill('black') 
+    line (140, 60, 180, 80);
+    line(250,80,200,80);
+    ;
+    fill('gold');
+    textSize(18); 
+    text ('Graham.M',300,380);
+            }
